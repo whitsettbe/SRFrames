084519917f3d617a46418765b22e876d56faecc0
whitsettbe <98466593+whitsettbe@users.noreply.github.com>
2022-06-16 18:20:38 -0400

    Mouse pan testing

--- srscript.js
+++ srscript.js
@@ -693,6 +693,17 @@ var prevPtrGap = 0;
 canv.addEventListener("pointerdown", function(event)
 {
 	pointCache.push(event);
+
+	//update center of pointers
+	oldMouseX = 0;
+	oldMouseY = 0;
+	for(var i = 0; i < pointCache.length; i++)
+	{
+		oldMouseX += pointCache[i].clientX;
+		oldMouseY += pointCache[i].clientY;
+	}
+	oldMouseX /= pointCache.length;
+	oldMouseY /= pointCache.length;
 }, false);
 
 //update moving pointer
